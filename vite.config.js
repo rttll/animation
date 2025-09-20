08a5b1b8fcf070e356a666511d18941a0edee6dc
adrian <adrianxyz@proton.me>
2025-09-19 22:35:05 -0700

    basic inver curosr

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         nested: resolve(__dirname, 'nested/index.html'),
+        invert: resolve(__dirname, 'cursors/invert/index.html'),
       },
     },
   },
